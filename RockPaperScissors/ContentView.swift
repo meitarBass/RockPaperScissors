cbf09344e81e11ca93cc70af119adff2880ce931
meitarbasson <meitarbass@gmail.com>
2022-07-04 09:22:36 +0300

    First commit

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State private var userChoice: Choices
+//    @State private var gameResult: Results
+    
+    private var playerScore: Int = 0
+    private var roundNumber: Int = 1
+    
+    enum Choices: String {
+        case rock = "🪨"
+        case paper = "🧻"
+        case scissors = "✂️"
+    }
+    
+    enum Results: Int {
+        case lose
+        case tie
+        case win
+    }
+    
+    let possibleChoices = [Choices.rock, Choices.paper, Choices.scissors]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("Round number: \(roundNumber)")
+            Text("Player's score is \(playerScore)")
+                .font(.system(size: 72))
+            Spacer()
+            HStack {
+                Spacer()
+                Button(Choices.rock.rawValue) {
+//                    userChoice = .rock
+                }
+                Spacer()
+                Button(Choices.paper.rawValue) {
+//                    userChoice = .paper
+                }
+                Spacer()
+                Button(Choices.scissors.rawValue) {
+//                    userChoice = .scissors
+                }
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
 
